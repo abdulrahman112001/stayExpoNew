4a4420a54c74705f309e235aa8c644849eae7a29
abdulrahman112001 <abdulrahmanelshikh2001@gmail.com>
2023-08-05 19:53:21 +0300

    prod3

--- pages/events/[id].tsx
+++ pages/events/[id].tsx
@@ -4,8 +4,9 @@ import React from "react";
 
 import Image from "next/image";
 import Link from "next/link";
-import AccordionComp from "@/component/accordion/Accordion";
-import Button from "@/component/atoms/Button";
+import AccordionComp from "../component/accordion/Accordion";
+import Button from "../component/atoms/Button";
+
 
 export default function Events() {
   return (
