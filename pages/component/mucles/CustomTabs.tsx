2a16159a6d923071f2d7a5a238f13fe2444f98d5
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2023-09-08 00:27:53 +0300

    d

--- pages/component/mucles/CustomTabs.tsx
+++ pages/component/mucles/CustomTabs.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Carousel } from "@mantine/carousel";
 import {
+  IconAward,
   IconBeach,
   IconBuilding,
   IconBuildingBank,
@@ -13,6 +14,7 @@ import {
   IconCoins,
   IconFileCertificate,
   IconFlag,
+  IconLocation,
   IconPinnedFilled,
   IconSeeding,
   IconSmokingNo,
